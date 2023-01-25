ab3f8c3de5de5fe5dc3785ef0b1b61eb0fc2083d
ChaoHuaH <chaohua77@outlook.com>
2023-01-25 12:47:46 -0600

    add QUnit.h

--- A1/Main/BufferMgr/headers/Clock_LRU.h
+++ A1/Main/BufferMgr/headers/Clock_LRU.h
@@ -1,13 +1,50 @@
 #ifndef CLOCK_LRU_H
 #define CLOCK_LRU_H
 
+#include <vector>
+
 using namespace std;
+
+
 // use Page_Buffer_Item as element
+// template <size_t numPages>
 class Clock_LRU
 {
 private:
+    Page_Buffer_Item *currentPagePtr;
+    Page_Buffer_Item *pageBuffer;
+    size_t numPages;
+
 
 public:
+	// numPages: the number of pages managed by the buffer manager
+    Clock_LRU (size_t numPages = 16, size_t pageSize) {
+        this->numPages = numPages;
+        for (size_t i=0; i<numPages, i++)
+        {
+            pageBuffer[i] = new Page_Buffer_Item(pageSize);
+        }
+        currentPagePtr = pageBuffer;
+    }
+
+    // when the page buffer is destroyed
+    // - disk page: 
+    // dirty pages need to be written back to disk, 
+    // any necessary data needs to be written to the catalog
+    // - anonymous page:
+    // |-- still have handle on it: written to a temporary file, and return the address
+    // |-- no handle: delete    
+    ~Clock_LRU ();
+
+    void evict()
+    void setAcedBit()
+
+    
+
+    
+
+    
+
 
 }
 
--- A1/Main/BufferMgr/headers/Page_Buffer_Item.h
+++ A1/Main/BufferMgr/headers/Page_Buffer_Item.h
@@ -14,11 +14,64 @@ private:
 	int refCnt;
 	size_t pageSize;
 	vector<char> pageData;
-	
-	
 
+	bool acedBit = false; // second change bit, default as unset(false)
+	
 
 public:
+	Page_Buffer_Item(size_t pageSize)
+	{
+		this->pageSize = pageSize;
+	}
+
+	// If page found in buffer, updates the acedBit to true
+    bool hitAndSet() {
+        acedBit = true;
+    }
+
+	// If page not found in buffer
+    // 1. when arm point to a setted item, unset & move arm to next item
+    // 2. when arm point to an unsetted item, load data & set & move arm
+    // (if point to a pinned pages, just move not next item)
+    // return the pointer to item, where the arm currently points to
+    Page_Buffer_Item * faultAndLoad(Page_Buffer_Item *currentPagePtr, 
+							Page_Buffer_Item newPageItem) {
+		// currentPage is null
+		// reset to head
+		// 要放在 Page_Buffer_Itme 還是 Clock_LRU?
+		if (currentPagePtr == nullptr)
+		{
+
+		}
+
+		while (true) 
+		{
+			// find the page to evict (acedBit == false)
+			if (!(*currentPagePtr->acedBit))
+			{
+				// if dirty
+				if (isDirty)
+				{
+					// Anonymous & no handle 
+					// delete
+					
+					// Anonymous & with handle
+					// save to a temp file
+
+					// disk: update disk page
+
+					
+				}
+				// evict & replace with newPageItem
+				*currentPagePtr = newPageItem;
+
+				// return updated pointer
+				return (currentPagePtr+1);
+			}
+			acedBit = true;
+			currentPagePtr++;
+		}
+	}
 
 }
 
