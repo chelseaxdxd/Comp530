1fd10e2352c68fc072fa01c91b57f0e613784cc9
chelsea <870313amy@gmail.com>
2023-01-29 16:49:17 -0600

    testing vectorCharPtr succeed

--- testing/vectorCharPtr.cpp
+++ testing/vectorCharPtr.cpp
@@ -8,11 +8,18 @@ int main()
     vector<char> pageData;
     pageData.push_back('a');
     pageData.push_back('b');
+    pageData.push_back('c');
     buff = &pageData[0];
     cout << buff << endl;
+    buff[0] = '1';
+    buff[1] = '2';
+    buff[2] = '3';
+    cout << pageData[0];
+    cout << pageData[1];
+    cout << pageData[2];
+
     return 0;
 }
-
 /*
 // char* buffer;
 // ...
